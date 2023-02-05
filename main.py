6dac0845e64223f17e77398496cbe919ccaee745
par1ram <114154230+par1ram@users.noreply.github.com>
2023-02-05 20:32:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import openai
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+token = '5929131995:AAFla4N5wYq0lD2fIE2Oke-GsSnnF-lNvaE'
+openai.api_key = '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'
+
+bot = Bot(token)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def send(message : types.Message):
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=message.text,
+    temperature=0.9,
+    max_tokens=1000,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.6,
+    stop=["You:"]
+)
+    await message.answer(response['choices'][0]['text'])
+
+executor.start_polling(dp, skip_updates=True)
